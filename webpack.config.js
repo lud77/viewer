6424cd040e12acc4742d9996b962d6e5164062d1
lud77 <stefano.butera@nethnic.com>
2018-03-30 16:40:44 +0100

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,39 @@
-const path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    entry: './src/client/main.js',
-    output: {
-        path: path.resolve(__dirname, 'dist/js'),
-        filename: 'script.min.js'
-    },
     module: {
-        loaders: [{
+        rules: [{
             test: /\.js$/,
-            include: path.join(__dirname, 'src'),
-            loader: 'babel-loader',
-            query: {
-                plugins: ['transform-runtime'],
-                presets: ['env']
+            exclude: /node_modules/,
+            use: {
+                loader: "babel-loader"
             }
+        }, {
+            test: /\.css$/,
+            exclude: /node_modules/,
+            use: {
+                loader: "css-loader"
+            }
+        }, {
+            test: /\.html$/,
+            use: [{
+                loader: "html-loader",
+                options: { minimize: true }
+            }]
+        }, {
+            test: /\.css$/,
+            use: [MiniCssExtractPlugin.loader, 'css-loader']
         }]
-    }
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/client/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+          filename: '[name].css',
+          chunkFilename: '[id].css'
+        })
+    ]
 };

deebd47e493f77d20e482116f8ee4af119c79fca
lud77 <stefano.butera@nethnic.com>
2018-03-30 12:38:00 +0100

    Replace viewer controller with static route

--- src/server/server.js
+++ src/server/server.js
@@ -1,17 +1,20 @@
 const express = require('express');
+const path = require('path');
 
-const viewerControllerFactory = require('../controllers/viewer');
+//const viewerControllerFactory = require('../controllers/viewer');
 
 const app = express();
 
-const { viewer } = viewerControllerFactory();
+//const { viewer } = viewerControllerFactory();
 
 app.disable('etag');
 
-app.get('/', viewer);
+app.use('/', express.static(path.join(__dirname, 'dist')))
 
-const server = app.listen(process.env.PORT || 29292, () => {
-  logger.info('Listening on port ' + server.address().port);
+//app.get('/', viewer);
+
+const server = app.listen(process.env.PORT || 80, () => {
+    console.log('Listening on port ' + server.address().port);
 });
 
 module.exports = server;

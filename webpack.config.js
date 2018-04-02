f35651ca686a2bbcf4d8d43aa5ea73a686ad68ef
lud77 <stefano.butera@nethnic.com>
2018-04-02 12:57:01 +0100

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     module: {
@@ -10,30 +10,18 @@ module.exports = {
                 loader: "babel-loader"
             }
         }, {
-            test: /\.css$/,
-            exclude: /node_modules/,
-            use: {
-                loader: "css-loader"
-            }
-        }, {
             test: /\.html$/,
+            exclude: /node_modules/,
             use: [{
                 loader: "html-loader",
                 options: { minimize: true }
             }]
-        }, {
-            test: /\.css$/,
-            use: [MiniCssExtractPlugin.loader, 'css-loader']
         }]
     },
     plugins: [
         new HtmlWebPackPlugin({
             template: './src/client/index.html',
             filename: './index.html'
-        }),
-        new MiniCssExtractPlugin({
-          filename: '[name].css',
-          chunkFilename: '[id].css'
         })
     ]
 };

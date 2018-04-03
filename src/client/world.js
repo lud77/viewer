e1bdd02ac176d39e629f4aebc61d72f29d51111e
lud77 <stefano.butera@nethnic.com>
2018-04-03 08:05:57 +0100

    Add scene object selection by click

--- src/client/world.js
+++ src/client/world.js
@@ -8,15 +8,22 @@ module.exports = () => {
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     const renderer = new THREE.WebGLRenderer();
     const controls = new OrbitControls(camera, renderer.domElement);
+    const raycaster = new THREE.Raycaster();
+    const mouse = new THREE.Vector2();
 
-    const objects = {};
+    let selected = null;
+
+    const objects = [];
 
     const add = (id, obj) => {
-        objects[id] = { obj };
+        obj.name = id;
+        objects.push({ id, obj });
         scene.add(obj);
     };
 
-    const get = (id) => objects[id];
+    const get = (id) => objects.find((el) => el.id === id);
+
+    const getAllObjects = () => objects.map((el) => el.obj);
 
     const setBackground = (background) => {
         scene.background = background;
@@ -44,10 +51,28 @@ module.exports = () => {
         renderer.setSize(w, h);
     };
 
+    const handleMouseDown = (event) => {
+        mouse.x = 2 * (event.clientX / window.innerWidth) - 1;
+        mouse.y = 1 - 2 * (event.clientY / window.innerHeight);
+
+        raycaster.setFromCamera(mouse, camera);
+
+        const intersects = raycaster.intersectObjects(scene.children);
+
+        if (intersects.length > 0) {
+            console.log(`intersected ${intersects[0].object.name}`, intersects[0]);
+            selected = intersects[0];
+        } else {
+            selected = null;
+        }
+    };
+
     document.body.appendChild(renderer.domElement);
 
     window.addEventListener('resize', handleResize, false);
     handleResize();
+
+    document.addEventListener('mousedown', handleMouseDown);
 
     add('camera', camera);
 

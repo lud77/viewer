869639224197179cf72f4cc792737bfca52f4ac7
Stefano Butera <stefano.butera@trinitymirror.com>
2018-04-04 19:37:53 +0100

    Add a maze and endpoint to serve it

--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 //const viewerControllerFactory = require('../controllers/viewer');
 
@@ -12,9 +13,12 @@ app.disable('etag');
 app.use('/', express.static(path.join(__dirname, '../../dist')));
 app.use('/assets', express.static(path.join(__dirname, '../../assets')));
 
-//app.get('/', viewer);
+app.get('/map', (req, res) => {
+    const map = fs.readFileSync(path.join(__dirname, '../../maps/maze5.txt')).toString('utf-8');
+    res.send(map);
+});
 
-const server = app.listen(process.env.PORT || 80, () => {
+const server = app.listen(process.env.PORT || 2327, () => {
     console.log('Listening on port ' + server.address().port);
 });
 
